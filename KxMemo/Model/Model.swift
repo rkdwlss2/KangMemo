ba069bd0770bf155370423996dad129f18ea64ce
kang <rkdwlss@gmail.com>
2020-02-16 19:22:30 +0900

    memoclass

---
+++ KxMemo/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  KxMemo
+//
+//  Created by 강명진 on 2020/02/16.
+//  Copyright © 2020 myoungjin. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content : String
+    var insertDate : Date
+    
+    init (content: String){
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "subscribe")
+    ]
+}
