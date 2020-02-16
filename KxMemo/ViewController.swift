d3b4fb361f92e5b67f50a6e45d67ac34cc7db366
kang <rkdwlss@gmail.com>
2020-02-16 18:49:17 +0900

    Initial Commit

---
+++ KxMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KxMemo
+//
+//  Created by 강명진 on 2020/02/16.
+//  Copyright © 2020 myoungjin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
