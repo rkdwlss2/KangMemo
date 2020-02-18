8f17c603bfa64d620a1ae931cc67d00553862fdd
kang <rkdwlss@gmail.com>
2020-02-18 18:06:12 +0900

    camera

--- KxMemo/ComposeViewController.swift
+++ KxMemo/ComposeViewController.swift
@@ -10,9 +10,47 @@ import UIKit
 
 class ComposeViewController: UIViewController {
     
+    let picker = UIImagePickerController()
     
     var editTarget: Memo?
     var originalMemoContent: String?
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func addAction(_ sender: Any) {
+        let alert = UIAlertController(title: "원하는 타이틀", message: "원하는 메세지", preferredStyle: .actionSheet)
+        let library = UIAlertAction(title: "사진앨범", style: .default){
+            (action) in self.openLibrary()
+        }
+        let camera = UIAlertAction(title: "카메라", style: .default){
+            (action) in self.openCamera()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(library)
+        
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    func openLibrary(){
+        picker.sourceType = .photoLibrary
+        present(picker,animated: false,completion: nil)
+    }
+    func openCamera(){
+        if (UIImagePickerController .isSourceTypeAvailable(.camera)){
+            picker.sourceType = .camera
+            present(picker,animated: false,completion: nil)
+        }
+        else{
+            print("Camera not available")
+        }
+        
+    }
     
     
     @IBAction func close(_ sender: Any) {
@@ -58,6 +96,7 @@ class ComposeViewController: UIViewController {
         }
         
         memoTextView.delegate=self
+        picker.delegate=self
         // Do any additional setup after loading the view.
     }
     
@@ -123,4 +162,17 @@ extension ComposeViewController: UIAdaptivePresentationControllerDelegate{
 extension ComposeViewController{
     static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
     static let memoDidChange = Notification.Name(rawValue: "memoDidChange")
+}
+
+extension ComposeViewController : UIImagePickerControllerDelegate,
+UINavigationControllerDelegate{
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image = image
+            //            imageView.image = image
+            print(info)
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
